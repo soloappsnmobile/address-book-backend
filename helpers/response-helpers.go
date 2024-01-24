4268539a8476d7043d6416e7044d514dbc51530c
soloappsnmobile <solomon@appsnmobilesolutions.com>
2024-01-24 09:28:10 +0000

    refactor: file name refactor

--- helpers/response-helpers.go
+++ helpers/response-helpers.go
@@ -1,9 +1,6 @@
 package helpers
 
 import (
-	"errors"
-	"strconv"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,12 +16,4 @@ func RespondWithSuccess(c *gin.Context, code int, message interface{}, respCode 
 	}
 
 	c.JSON(code, response)
-}
-
-func ConvertStringToFloat64(s string) (float64, error) {
-	value, err := strconv.ParseFloat(s, 64)
-	if err != nil {
-		return 0, errors.New("conversion failed")
-	}
-	return value, nil
 }
