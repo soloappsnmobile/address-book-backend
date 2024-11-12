a4617fafa170247afc642dabef553ee33ed0a529
mcbryan1 <mcbryansolomon@yahoo.com>
2024-11-12 08:20:07 +0000

    Fixed Cors Issues

--- routers/router.go
+++ routers/router.go
@@ -3,11 +3,14 @@ package routers
 import (
 	"address-book-backend/controllers"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+
+	applyCORS(router)
 
 	router.POST("/address-books", controllers.CreateAddressBook)
 	router.GET("/address-books", controllers.GetAddressBooks)
@@ -15,4 +18,12 @@ func SetupRouter() *gin.Engine {
 	router.PUT("/address-book/:id", controllers.UpdateAddressBook)
 	router.DELETE("/address-book/:id", controllers.DeleteAddressBook)
 	return router
+}
+
+func applyCORS(router *gin.Engine) {
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
+	config.AllowHeaders = []string{"Content-Type", "Authorization", "Accept", "Accept-Version", "Origin"}
+	router.Use(cors.New(config))
 }
